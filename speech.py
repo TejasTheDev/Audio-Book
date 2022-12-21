99dee621435da8d3c5cdc0add27b0cbbade51564
tejas <tejas.github2k@gmail.com>
2022-12-21 10:51:56 +0530

    push

---
+++ speech.py
@@ -0,0 +1,24 @@
+# import pyttsx3
+# import PyPDF2
+
+# book = open('D:\projects\Audio Book\python.pdf', 'rb')
+# pdfReader = PyPDF2.PdfFileReader(book)
+# pages = pdfReader.numPages
+# print("Total pages")
+# print(pages)
+
+# speaker = pyttsx3.init()
+# for num in range(10, pages):
+#     page = pdfReader.getPage(10)
+#     text = page.extract_text()
+#     speaker.say(text)
+#     speaker.runAndWait()
+
+
+
+
+
+import pyttsx3   
+speaker = pyttsx3.init()
+speaker.say("i thrust u")
+speaker.runAndWait()
